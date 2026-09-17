b91e3e90821100f50ab603471159ace50e141254
ikhwanmuhammadikhwan7-cyber <ikhwanmuhammadikhwan7@gmail.com>
2026-09-17 09:48:33 +0700

    Update pengaturan.js

--- pengaturan.js
+++ pengaturan.js
@@ -8,6 +8,6 @@ const CONFIG_APP = {
     namaVendor: "id.studiophoto",
 
     // 2. Google Drive API Key (Penting agar galeri bisa memuat foto)
-    googleApiKey: "AIzaSyAZczbLeVTXl-QKqLDJFWQLCd-lW9jqqxo",
+    googleApiKey: "AIzaSyBH6suFjEKgtONOazU_myhrmr8b2SbSavI",
     whatsappAdmin: "62821000000000" // <-- TAMBAHKAN BARIS INI (Ganti dengan nomor WA admin)
 };
